b6df9ae366783f70938896c6b568d659168267bd
Your Name <you@example.com>
2023-11-26 14:00:38 -0800

    added 3 functions to engine.cpp

---
+++ code/Engine.cpp
@@ -0,0 +1,19 @@
+#include "Engine.h"
+#include <iostream> // might not be needed
+
+Engine::Engine() : m_Window(VideoMode(1920, 1080), "Particles!!", Style::Default)
+{
+
+}
+
+void Engine::input()
+{
+
+}
+
+void Engine::update(float dtAsSeconds)
+{
+
+}
+
+
